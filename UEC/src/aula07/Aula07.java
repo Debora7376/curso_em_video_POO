7322f140d42802aa84afdbc1f7bed5854f17232c
debora7376 <devsouza.root@gmail.com>
2021-04-19 16:56:51 -0300

    aula 7A

---
+++ UEC/src/aula07/Aula07.java
@@ -0,0 +1,11 @@
+package aula07;
+import java.util.List;
+
+public class Aula07 {
+
+    public static void main(String[] args) {
+       Lutador lutadores[] = new Lutador [8];
+
+      lutadores[0] =  new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9, 11, 3,1);
+      }
+    }
