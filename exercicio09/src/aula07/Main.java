406534acdee54247530acd1b734bf6928cae258a
debora7376 <devsouza.root@gmail.com>
2021-04-30 16:50:22 -0300

    todo

--- exercicio09/src/aula07/Main.java
+++ exercicio09/src/aula07/Main.java
@@ -3,12 +3,12 @@ package aula07;
 public class Main  {
 
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.setNome("Debora");
-        System.out.println(pessoa1);
-        pessoa1.setIdade(24);
-        pessoa1.setSexo("F");
-
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Debora");
+        // TODO: 30/04/2021 rever o erro do retorno do objeto pessoa 
+        System.out.println(pessoa);
+        pessoa.setIdade(24);
+        pessoa.setSexo("F");
         Livro livro = new Livro();
         livro.setAberto(true);
         System.out.println(livro.getAberto());
@@ -16,7 +16,7 @@ public class Main  {
         System.out.println(livro.getTotalPaginas());
         livro.setTitulo("Abolição do homem");
         System.out.println(livro.getTitulo());
-        livro.setLeitor(pessoa1);
+        livro.setLeitor(pessoa);
         System.out.println(livro.getLeitor());
        // livro.folhear();
     }
