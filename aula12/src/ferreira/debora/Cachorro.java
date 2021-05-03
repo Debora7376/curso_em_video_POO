2b09cca8e55d5bef28732ec31d22ad0a422a4f50
debora7376 <devsouza.root@gmail.com>
2021-05-03 16:50:56 -0300

    adicionado aulas

--- aula12/src/ferreira/debora/Cachorro.java
+++ aula12/src/ferreira/debora/Cachorro.java
@@ -1,6 +1,18 @@
 package ferreira.debora;
 
 public class Cachorro extends Manifero{
+    private String corPelo;
+
+    @Override
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    @Override
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
     public void enterrarOsso(){
         System.out.println("Enterrando o osso");
     }
